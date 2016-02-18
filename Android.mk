c85bc1c8e5f132db7818206f4781884a9d8bffec
corphish <d97.avinaba@gmail.com>
2016-02-18 22:45:10 +0530

    cpufreq: Make it build with android build environment

---
+++ Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := cpufreq-sc8810.ko
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/modules
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+
+$(LOCAL_PATH)/cpufreq-sc8810.ko: bootimage
+	$(MAKE) -C $(shell dirname $@) ARCH=arm CROSS_COMPILE=$(ARM_EABI_TOOLCHAIN)/arm-eabi- KDIR=$(ANDROID_PRODUCT_OUT)/obj/KERNEL_OBJ
